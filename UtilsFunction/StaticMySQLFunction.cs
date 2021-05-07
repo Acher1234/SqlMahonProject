fc1ed4d850748eaeca2e728a8f0817b211d35aea
Acher1234 <Acherklein0@gmail.com>
2021-05-07 10:19:20 +0300

    test

--- UtilsFunction/StaticMySQLFunction.cs
+++ UtilsFunction/StaticMySQLFunction.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace SqlMahonProject.UtilsFunction
@@ -11,11 +13,27 @@ namespace SqlMahonProject.UtilsFunction
         public static string database = "mahonnew";
         public static string uid = "root";
         public static string password = "";
+        private static object dataGrid;
 
         public static List<string>  GetHotelID()
         {
-            
-        
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "PORT=" + port + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            string CmdString = string.Empty;
+            try
+            {
+                MySqlConnection con = new MySqlConnection(connectionString);
+                CmdString = "SELECT * FROM hotel";
+                MySqlCommand cmd = new MySqlCommand(CmdString, con);
+                
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return  new List<string> { "Hotel", "Rooms", "Link", "test" };
+
         }
     }
 }
